4ce95a57a3671d5b27afe8b0462efacd4deba3d4
URUMI1 <s1310153@u-aizu.ac.jp>
2025-07-01 12:22:15 +0900

    Greetings added

--- dice_game.py
+++ dice_game.py
@@ -1,10 +1,11 @@
 import random
 
-print("Rolling dice...")
+name = input("What is your name?\n> ")
+print(f"Hello, {name}!")
 
+print("Rolling dice...")
 die1 = random.randint(1, 6)
 die2 = random.randint(1, 6)
-
 print(f"Die 1: {die1}")
 print(f"Die 2: {die2}")
 
